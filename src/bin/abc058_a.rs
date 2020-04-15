ecae6863cb46fddafc7678b93c154903f59a3e0b
yagince <straitwalk@gmail.com>
2020-04-15 22:26:58 +0900

    [#master] 気分転換

---
+++ src/bin/abc058_a.rs
@@ -0,0 +1,28 @@
+use std::io::prelude::*;
+
+fn input<T>() -> T
+where
+    T: std::str::FromStr,
+{
+    let stdin = std::io::stdin();
+    let token: String = stdin
+        .lock()
+        .bytes()
+        .map(|c| c.unwrap() as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().unwrap()
+}
+
+fn main() {
+    let a = input::<i32>();
+    let b = input::<i32>();
+    let c = input::<i32>();
+
+    if b - a == c - b {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+}
